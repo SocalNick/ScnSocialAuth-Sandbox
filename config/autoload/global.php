c23f3861b7b64f26e4d7e4c861c338633636c7a5
Nicholas Calugar <njcalugar@gmail.com>
2014-06-12 06:18:54 +0000

    Configuring to run on Pivotal Web Services

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -11,6 +11,29 @@
  * file.
  */
 
+$vcapServices = \Zend\Json\Json::decode($_ENV['VCAP_SERVICES'], \Zend\Json\Json::TYPE_ARRAY);
+$clearDbCreds = $vcapServices['cleardb'][0]['credentials'];
+
 return array(
-    // ...
+    'db' => array(
+        'driver'    => 'PdoMysql',
+        'hostname'  => $clearDbCreds['hostname'],
+        'database'  => $clearDbCreds['name'],
+        'username'  => $clearDbCreds['username'],
+        'password'  => $clearDbCreds['password'],
+    ),
+    'scn-social-auth' => array(
+        'facebook_client_id' => $_ENV['facebook_client_id'],
+        'facebook_secret' => $_ENV['facebook_secret'],
+        'twitter_consumer_key' => $_ENV['twitter_consumer_key'],
+        'twitter_consumer_secret' => $_ENV['twitter_consumer_secret'],
+    ),
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory',
+        ),
+        'invokables' => array(
+            'Zend\Session\SessionManager' => 'Zend\Session\SessionManager',
+        ),
+    ),
 );
